e2c2941cbd89d90554b01e41d074f8005507159f
Raskin Vladimir <raskin_v@mail.ru>
2023-11-08 12:52:39 +0000

    Project done

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.74.12', 'localhost', '127.0.0.1', 'kittygramya.hopto.org']
 
